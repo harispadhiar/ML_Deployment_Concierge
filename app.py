68c7b57a589f5f7837a44f62e9378cfc3fca01bb
Haris Padhiar <padhiar.haris@gmail.com>
2026-07-06 22:17:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -203,4 +203,4 @@ def build_interface():
 
 if __name__ == "__main__":
     demo = build_interface()
-    demo.launch(server_name="127.0.0.1", server_port=7860, theme=THEME, css=CSS)
+    demo.launch(server_name="0.0.0.0", server_port=7860, theme=THEME, css=CSS)
